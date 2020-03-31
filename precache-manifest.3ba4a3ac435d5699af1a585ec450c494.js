2cc8d12240135896670c0ae1027150d9cc87e5c0
Jan Mager <jasiu.mager@gmail.com>
2020-03-31 16:21:46 +0200

    Updates

---
+++ precache-manifest.3ba4a3ac435d5699af1a585ec450c494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fde6db1b909c6b67f96c34d9b660c1",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "a30681f0beecdc0a8577",
+    "url": "/covid/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "53b6fd3e615b2a8d13bd",
+    "url": "/covid/static/css/main.a31fdd64.chunk.css"
+  },
+  {
+    "revision": "a30681f0beecdc0a8577",
+    "url": "/covid/static/js/2.e28f3ea5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid/static/js/2.e28f3ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b6fd3e615b2a8d13bd",
+    "url": "/covid/static/js/main.28be0cd6.chunk.js"
+  },
+  {
+    "revision": "5427f3d591adcdb4afa0",
+    "url": "/covid/static/js/runtime-main.52861baf.js"
+  }
+]);
